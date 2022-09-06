901f58800780d35d638b2eeebc16b168f30af937
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-09-06 01:45:54 -0400

    adding navigation component and styles to nav component also create home page component

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,17 +1,7 @@
 import Home from './routes/home/home.component';
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
 
 const Shop = () => {
   return (
@@ -23,7 +13,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route path='home' element={<Home />} />
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
