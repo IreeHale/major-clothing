564b6a3d3ce7870fdcc22e8c83af18167ff6801b
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-09-21 20:41:09 -0400

    adding context files to project and sign in component

--- clothing/src/components/sign-in-form/sign-in-form.component.jsx
+++ clothing/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,9 @@
 import './sign-in-form.styles.scss';
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword} from '../../routes/utils/firebase/firebase.utils';
 import FormInput from "../form-input/form-input.component";
 import Button from '../button/button.component';
+import  { UserContext } from '../../context/user.context';
 
 
 const defaultformFields = {
@@ -15,7 +16,7 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultformFields);
     const { email, password } = formFields;
     
-    console.log(formFields);
+   const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultformFields);
@@ -30,8 +31,13 @@ const SignInForm = () => {
         event.preventDefault();
 
         try{
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            const { user } = await signInAuthUserWithEmailAndPassword(
+                email, 
+                password
+                );
+
+                setCurrentUser(user);
+
             resetFormFields();
         } catch(error) {
             switch (error.code){
