162c6d75a4d20d1f6cd56b65f6fe269e80ba8d4d
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-09-27 04:32:20 -0400

    adding toggle functionality to cart icon

--- clothing/src/routes/navigation/navigation.component.jsx
+++ clothing/src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,15 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import { UserContext } from '../../context/user.context';
- import { signOutUser } from '../../routes/utils/firebase/firebase.utils.jsx';
+import { signOutUser } from '../../routes/utils/firebase/firebase.utils.jsx';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartContext } from '../../context/cart.context';
 
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-
+  const { isCartOpen } = useContext(CartContext);
 
     return (
       <Fragment>
@@ -20,16 +23,17 @@ const Navigation = () => {
             <Link className='nav-link' to='/Shop'>
                 Shop  
             </Link>
-            {
-              currentUser ? (
+            
+              {currentUser ? (
                 <span className='nav-link' onClick={signOutUser}> SIGN OUT </span>
                 ) :  ( 
                   <Link className='nav-link' to='/auth'>
                     Sign-In
                   </Link>
-                )
-              }
+                )}
+                <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
